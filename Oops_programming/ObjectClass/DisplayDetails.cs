dd346aab0a3bfd825a35486d9a57545de23ce912
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-05 14:59:31 +0530

    [narendra] write a program to display detail

--- Oops_programming/ObjectClass/DisplayDetails.cs
+++ Oops_programming/ObjectClass/DisplayDetails.cs
@@ -6,5 +6,20 @@ namespace ObjectClass
 {
     internal class DisplayDetails
     {
+        int id;
+        string name;
+        int age;
+        public DisplayDetails(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public void display()
+        {
+            Console.WriteLine("id = " + id);
+            Console.WriteLine("name = " + name);
+            Console.WriteLine("age = " + age);
+        }
     }
 }
