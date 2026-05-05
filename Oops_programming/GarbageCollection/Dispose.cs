e4aac99caad941b42547bdc2b415001c625b369d
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-05 16:01:55 +0530

    [narendra] implement dispose method

--- Oops_programming/GarbageCollection/Dispose.cs
+++ Oops_programming/GarbageCollection/Dispose.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace GarbageCollection
+class FileManager : IDisposable
 {
-    internal class Dispose
+    public void OpenFile()
     {
+        Console.WriteLine("File Opened");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Resources Released");
+    }
+}
+
+class Dispose
+
+{
+    static void Main()
+    {
+        using (FileManager fm = new FileManager())
+        {
+            fm.OpenFile();
+        } // Dispose automatically called here
     }
 }
