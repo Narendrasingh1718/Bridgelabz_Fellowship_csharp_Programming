6b280edfa06141d5fe7e7446599f9a95bddfa366
Narendra singh <narendrasingh21208@gmail.com>
2026-05-05 14:16:00 +0530

    [Narendra Singh] Add. a program file

--- Oops_programming/ObjectClass/Program.cs
+++ Oops_programming/ObjectClass/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿﻿namespace ObjectClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DisplayDetails d = new DisplayDetails(1, "Rohit", 18);
+            //d.display();
+            //TravelDetails t= new TravelDetails("Pune", "Mumbai", 150);
+            BookTitle b = new BookTitle("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+            b.display();
+
+        }
+    }
+  }
