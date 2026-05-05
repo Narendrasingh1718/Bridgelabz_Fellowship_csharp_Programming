5f425527c98b0edcbec78c61969a625c03085a07
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-05 14:10:20 +0530

    [narendra] write a program for booktitle

---
+++ Oops_programming/ObjectClass/BookTitle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectClass
+{
+    internal class BookTitle
+    {
+        string title;
+        string author;
+        double price;
+        public BookTitle(string title, string author, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+}
