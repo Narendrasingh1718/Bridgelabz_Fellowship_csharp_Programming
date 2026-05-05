48b48b82b600a2a642aff193a8f3e51657123963
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-05 16:12:21 +0530

    [narendra] implement Destructor

--- Oops_programming/GarbageCollection/Destructor.cs
+++ Oops_programming/GarbageCollection/Destructor.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Demo
+{
+    public Demo()
+    {
+        Console.WriteLine("Object Created");
+    }
+
+    ~Demo() // Finalizer (Destructor)
+    {
+        Console.WriteLine("Object Destroyed by GC");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo obj = new Demo();
+
+        obj = null; // Eligible for garbage collection
+
+        GC.Collect();       // Force GC (not recommended in real apps)
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("End of Main");
+    }
+}
