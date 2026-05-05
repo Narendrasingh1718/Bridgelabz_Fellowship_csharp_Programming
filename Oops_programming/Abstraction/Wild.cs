73bfbadf3801f3fcdbfd1ec48b3c198fea655d0e
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-05 16:09:21 +0530

    [narendra] learn abstraction

--- Oops_programming/Abstraction/Wild.cs
+++ Oops_programming/Abstraction/Wild.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Abstract class
+abstract class Animal
+{
+    // Abstract method (no body)
+    public abstract void Sound();
+
+    // Normal method
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping...");
+    }
+}
+
+// Derived class
+class Dog : Animal
+{
+    // Implement abstract method
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Wild
+{
+    static void Main()
+    {
+        Animal obj = new Dog();
+
+        obj.Sound();  // Dog barks
+        obj.Sleep();  // Animal is sleeping...
+    }
+}
