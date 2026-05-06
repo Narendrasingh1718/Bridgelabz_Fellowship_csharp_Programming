391e57255e4aedb685e35b12ddbe8323a2e955a1
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-06 14:42:41 +0530

    [narendra] Add. solve problem by using object as a datatype

--- Advance_Programming/Generics/Object.cs
+++ Advance_Programming/Generics/Object.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//namespace GenericsDemo
+//{
+//    public class ClsMain // by using object data type we can accept any data type but it is not a good practice because we have to do type casting and it is not type safe
+//    {
+//        private static void Main()
+//        {
+//            // bool IsEqual = ClsCalculator.AreEqual(10, 20);
+//            bool IsEqual = ClsCalculator.AreEqual("ABC", "ABC");
+//            if (IsEqual)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
 
-namespace Generics
-{
-    internal class @object
-    {
-    }
-}
+//    public class ClsCalculator
+//    {
+//        //Now this method can accept any data type
+//        public static bool AreEqual(object value1, object value2)
+//        {
+//            return value1 == value2;
+//        }
+//    }
+//}
