5968616b7767c436b0ef4e77641f23eca721d1c9
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-13 17:17:27 +0530

    [narendra] Email valid regex code

--- Advance_Programming/MakerPlan/Program.cs
+++ Advance_Programming/MakerPlan/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Email: ");
+
+        string email = Console.ReadLine();
+
+        string pattern =
+            @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        if (Regex.IsMatch(email, pattern))
+        {
+            Console.WriteLine("Valid Email");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Email");
+            Console.WriteLine("");
+
+        }
+    }
+}
