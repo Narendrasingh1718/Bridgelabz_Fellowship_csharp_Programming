ba09fac8e92b9a6a727448e0413cf7d3b11d4a94
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-13 16:59:34 +0530

    [narendra] Email valid regex code

--- Advance_Programming/Generics/GenericMethod.cs
+++ Advance_Programming/Generics/GenericMethod.cs
@@ -1,30 +1,30 @@
-﻿using System;
-namespace GenericsDemo
-{
-    public class GenericMethod // we solve above problem by using generic method which can accept any data type.
-    {
-        private static void Main()
-        {
-            //bool IsEqual = ClsCalculator.AreEqual<int>(10, 20); // we have to pass the data type when we call that method.
-            //bool IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
-            bool IsEqual = ClsCalculator.AreEqual<double>(10.5, 20.5);
-            if (IsEqual)
-            {
-                Console.WriteLine("Both are Equal");
-            }
-            else
-            {
-                Console.WriteLine("Both are Not Equal");
-            }
-            Console.ReadKey();
-        }
-    }
+﻿//using System;
+//namespace GenericsDemo
+//{
+//    public class GenericMethod // we solve above problem by using generic method which can accept any data type.
+//    {
+//        private static void Main()
+//        {
+//            //bool IsEqual = ClsCalculator.AreEqual<int>(10, 20); // we have to pass the data type when we call that method.
+//            //bool IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+//            bool IsEqual = ClsCalculator.AreEqual<double>(10.5, 20.5);
+//            if (IsEqual)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
 
-    public class ClsCalculator
-    {
-        public static bool AreEqual<T>(T value1, T value2) // T is replace with actual data type at the time of compilation. we can use any name instead of T but by convention we use T for generic type.
-        {
-            return value1.Equals(value2);
-        }
-    }
-}
+//    public class ClsCalculator
+//    {
+//        public static bool AreEqual<T>(T value1, T value2) // T is replace with actual data type at the time of compilation. we can use any name instead of T but by convention we use T for generic type.
+//        {
+//            return value1.Equals(value2);
+//        }
+//    }
+//}
