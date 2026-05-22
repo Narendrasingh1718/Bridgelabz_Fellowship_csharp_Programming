473d57ab27f0daf16094c23c149bfb8cebc9b35b
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:48:09 +0530

    [Narendra Singh] Add how to create json object on c#

---
+++ IO_Stream/File_handling/Json2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace File_handling
+{
+    public class Json2
+    {
+        public static void NestedJson()
+        {
+            var nestedJsonObject = new
+            {
+                Name = "narendra",
+            RollNo = "001",
+            Grade = "A",
+            Address = new
+            {
+                Street = "123 Main St",
+                City = "Anytown",
+                State = "CA",
+                ZipCode = "12345"
+            }
+        };
+    }
+}
