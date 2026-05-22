a8d8bedf62703d9d9c51e4106927ed848c17ec23
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:06:02 +0530

    [Narendra Singh] Add Task<T> class

---
+++ IO_Stream/Asynchronous_Programming/TaskOfT.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asynchronous_Programming
+{
+     public class TaskOfT
+    {
+        public static async Task<Weather> Get(String city) 
+        {
+            await Task.Delay(1000);
+            var weather = new Weather
+            {
+                City = city,
+                date = DateTime.Now,
+                Temprature = new Random().Next(-20, 40)
+            };
+            return weather;
+        }
+        public  static void TaskStatus(TaskStatus status)
+        {
+            Console.WriteLine(status);
+        }
+    }
+}
