310f1be1fc02aba4f97fc2e9b5f362d41d34a5a5
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 10:53:14 +0530

    [Narendra Singh] Add  program class

---
+++ IO_Stream/Attribute/Program.cs
@@ -0,0 +1,33 @@
+﻿using Custom;
+using Data;
+using Emp;
+using Log;
+using Model;
+using Serv;
+using System;
+using System.Reflection;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Service service = new Service();
+        //Type type = service.GetType();
+        //MethodInfo methodInfo = type.GetMethod("D0work");
+        //if(methodInfo.GetCustomAttribute(typeof(LogExecutionTimeAttribute)) is LogExecutionTimeAttribute attribute)
+        //{
+        //   var Started = DateTime.Now;
+        //    service.D0work();
+        //    var Ended = DateTime.Now;
+        //    Console.WriteLine($"{attribute.Message} {Ended - Started}");
+        //}
+        Employee2 emp = new Employee2();
+        Type type = emp.GetType();
+        MethodInfo methodInfo = type.GetMethod("Display");
+        if (methodInfo.GetCustomAttribute(typeof(DataAnnotation)) is DataAnnotation attribute)
+        {
+            Console.WriteLine($"{string.Join("Narendra, ", attribute.arr)} {attribute.no} ");
+            emp.Display();
+        }
+    }
+}
