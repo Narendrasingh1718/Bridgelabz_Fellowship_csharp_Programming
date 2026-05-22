63101c677257c2ace5c2c2c66d5be1256ef5bee5
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:45:42 +0530

    [Narendra Singh] Add how to read  a file using file csv reader

---
+++ IO_Stream/File_handling/Csv_file_reader.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace File_handling
+{
+    public class Csv_file_reader
+    {
+        static string path = "Student.csv";
+        public static void Reader()
+        {
+            try
+            {
+                using (var reader = new StreamReader(path)) 
+                using (var csv=new CsvReader(reader,CultureInfo.InvariantCulture)) 
+                {
+                 var records=csv.GetRecords<Student>();
+                    foreach(var record in records) 
+                    {
+                        Console.WriteLine($"Name: {record.Name}, RollNo: {record.RollNo}, Grade: {record.Grade}");
+                    }
+                }
+            }
+            catch(Exception ex) { 
+                Console.WriteLine(" " + ex.Message);    
+            }   
+        }
+    }
+}
