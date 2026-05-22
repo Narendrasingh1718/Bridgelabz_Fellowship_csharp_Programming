e581ebf9d9054ebaf1b0eba0bfb1caa56b45f0bf
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 10:58:18 +0530

    [Narendra Singh] Add LogExecutionAttribute custom attribute  class

---
+++ IO_Stream/Attribute/LogExecutionTimeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Log
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class LogExecutionTimeAttribute: Attribute
+    {
+        public String Message { get; set; }
+       
+
+        public LogExecutionTimeAttribute(string message)
+        {
+            Message = message;
+        }
+    }
+}
