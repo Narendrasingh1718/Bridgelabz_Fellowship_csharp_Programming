e62316f7b034d9b830919ee744d7caaa84b1e25b
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:13:00 +0530

    [Narendra Singh] Add how to create thread class

---
+++ IO_Stream/Multithearding/Createthread.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    internal class Createthread
+    {
+
+        public void run()
+        {
+            for (int i=0;i<10;i++)
+            {
+                Console.WriteLine("Child thread is running"+i);
+            }
+            
+        }
+    }
+}
