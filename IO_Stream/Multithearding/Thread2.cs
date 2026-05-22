bbaf4bbb6c363ef00f1d4bfa3bd5c0d6b5ab4f33
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:38:05 +0530

    [Narendra Singh] Add how to create thread

---
+++ IO_Stream/Multithearding/Thread2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    internal class Thread2
+    {
+       public void print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Narendra" + i);
+            }
+        }
+    }
+}
