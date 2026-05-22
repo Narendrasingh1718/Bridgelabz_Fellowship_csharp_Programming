30175c94ed31382da6766f01ce92e9f4aa86dd91
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 10:56:38 +0530

    [Narendra Singh] Add  custom attribute  class

---
+++ IO_Stream/Attribute/CustomAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class CustomAttribute : Attribute
+    {
+        public  string Name { get; set; }
+        public  string empno { get; set; }
+
+      public  CustomAttribute(string name, string empno)
+        {
+            Name = name;
+            this.empno = empno;
+        }
+    }
+}
