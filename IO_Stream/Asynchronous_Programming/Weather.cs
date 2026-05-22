0e8750ac5c8ac648dc459362d166d5312275aeb0
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:06:55 +0530

    [Narendra Singh] Add Weather class

---
+++ IO_Stream/Asynchronous_Programming/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asynchronous_Programming
+{
+    public class Weather
+    {
+        public String? City { get; set; }
+        public DateTime date { get; set; }
+        public double Temprature { get; set; }
+
+    }
+}
