f6a0f2e3a2459853d0188636a530d37770ddf96b
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:19:54 +0530

    [Narendra Singh] Add how to apply join

---
+++ IO_Stream/Multithearding/Join.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    public class Join
+    {
+        public void run()
+        {
+           for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("join"+i);
+            }
+        }
+    }
+}
