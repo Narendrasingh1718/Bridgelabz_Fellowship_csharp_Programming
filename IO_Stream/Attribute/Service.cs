8d9ef5e7b0d0b7d8f8a8a4e430ea2efa5fe8af10
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 10:54:48 +0530

    [Narendra Singh] Add  Service class

---
+++ IO_Stream/Attribute/Service.cs
@@ -0,0 +1,16 @@
+﻿using Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serv
+{
+    public class Service
+    {
+        [LogExecutionTime("work is done in more time  :")]
+        public  void D0work()
+        {
+            Console.WriteLine("Doing work...");
+        }
+    }
+}
