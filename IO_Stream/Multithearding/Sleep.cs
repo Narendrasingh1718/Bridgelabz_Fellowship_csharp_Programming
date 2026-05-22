cba4831112e675564175acf4416c9b6b7012be8c
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:20:24 +0530

    [Narendra Singh] Add how to apply Sleep

---
+++ IO_Stream/Multithearding/Sleep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    public class Sleep
+    {
+        public void run()
+        {
+            for(int i=0;i<10; i++)
+            {
+                Console.WriteLine("Sleep" + i); 
+                Thread.Sleep(1000);
+            }   
+        }
+    }
+}
