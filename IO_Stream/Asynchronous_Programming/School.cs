dc6af4086254a81f7b6d90a33298f7df9abf10c4
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:04:17 +0530

    [Narendra Singh] Add sync School class

---
+++ IO_Stream/Asynchronous_Programming/School.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asynchronous_Programming
+{
+    public class School
+    {
+        public void Prayer()
+        {
+            Console.WriteLine("Prayer started");
+            Thread.Sleep(4000);
+        }
+        public void Class()
+        {
+                Console.WriteLine("Class started");
+                Thread.Sleep(2000);
+        }
+        public void Class2()
+        {
+            Console.WriteLine("Class2 started");
+             Thread.Sleep(3000);
+        }
+    }
+}
