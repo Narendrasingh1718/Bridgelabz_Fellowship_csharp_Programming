9cae850ffe4301a7daff5c422b09523de78832fe
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 10:51:30 +0530

    [Narendra Singh] Add Read data class

---
+++ IO_Stream/Ado.Net/Read.cs
@@ -0,0 +1,35 @@
+﻿   using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO_DotNET
+{
+    internal class Read
+    {
+
+        public void ReadData()
+        {
+            String command = "Select *  from ado.employee";
+            try
+            {
+                using (MySqlConnection con = Connection_Class.GetConnection())
+                {
+                    con.Open();
+                    MySqlCommand cmd = new MySqlCommand(command, con);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader["id"]}, Name: {reader["name"]}, Age: {reader["age"]}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
