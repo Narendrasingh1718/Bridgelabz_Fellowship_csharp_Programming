45bae89298c83a3f36a554f1b9dcf397beaba9eb
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:20:48 +0530

    [Narendra Singh] Add how to apply Yeild

---
+++ IO_Stream/Multithearding/Yeild.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    public class Yeild 
+    {
+        public void run()
+        {
+            for(int i=0;i<10; i++)
+            {
+                Console.WriteLine("yeild" + i);
+                Thread.Yield();
+            }
+        }
+        
+    }
+}
