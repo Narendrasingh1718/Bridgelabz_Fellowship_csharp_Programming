da6e6b8ad434c6d23aed179e9fb09e031eb55027
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:01:43 +0530

    [Narendra Singh] Add Program class

---
+++ IO_Stream/Asynchronous_Programming/Program.cs
@@ -0,0 +1,34 @@
+﻿using Asynchronous_Programming;
+using System;
+
+class Program
+{
+    static async Task Main()
+    {
+        //SyncMethod sm = new SyncMethod();
+        //sm.run();
+        //AsyncMethod sm2 = new AsyncMethod();
+        //sm2.run();  
+
+        //School school = new School();
+        //school.Prayer();
+        //school.Class();
+        //school.Class2();
+
+        //AsyncSchool school2 = new AsyncSchool();    
+        //await school2.run();
+        //try
+        //{
+        //    var task = TaskOfT.Get("Tehran");
+        //    TaskOfT.TaskStatus(task.Status);
+        //    await task;
+        //    TaskOfT.TaskStatus(task.Status);
+             
+        //}
+        //catch (Exception ex) { 
+        //    Console.WriteLine(ex.ToString());
+        //}
+
+        
+    }   
+}
