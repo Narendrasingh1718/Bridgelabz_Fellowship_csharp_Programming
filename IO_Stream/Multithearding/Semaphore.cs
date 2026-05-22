3181565f675d0ebad394163e871408dd3c20182d
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:37:17 +0530

    [Narendra Singh] Add how to do Syncronize a programby usin semaphore

---
+++ IO_Stream/Multithearding/Semaphore.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Multithearding
+//{
+//    public class Semaphore
+//    {
+//        private static Semaphore s = new Semaphore(2,2);
+//        public void run()
+//        {
+
+//        }
+
+//    }
+//}
