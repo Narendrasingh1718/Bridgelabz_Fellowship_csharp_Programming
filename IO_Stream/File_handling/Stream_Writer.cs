e9f6ca1b4b203dafa7899433830194f6dac2f6a7
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:41:06 +0530

    [Narendra Singh] Add how to create a write a file using fil writer

---
+++ IO_Stream/File_handling/Stream_Writer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+    class Stream_Writer
+    {
+        public static void Start()
+        {
+            try
+            {
+                string path = @"C:\Users\naren\Documents\file.txt";
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    Console.WriteLine("Enter text:");
+                    string input = Console.ReadLine();
+
+                    sw.WriteLine(input);
+                }
+
+                Console.WriteLine("✅ File created successfully at: " + path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("❌ Error: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
