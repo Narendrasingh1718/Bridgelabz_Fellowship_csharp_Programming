4e9f9414d56dc9c978cb1b5875ee8b5c9346ad28
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:16:08 +0530

    [Narendra Singh] Add how to create thread  by using ParameterizedThread  class

---
+++ IO_Stream/Multithearding/PrameterizedThread.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithearding
+{
+    public static class PrameterizedThread
+    {
+        public static void run(object name)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + " " + name);
+            }
+        }
+    }
+}
