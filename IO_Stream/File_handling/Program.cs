c08234312a70d92f1d163fb201a37ffbbf3c0108
Narendra Singh <narendrasingh21208@gmail.com>
2026-05-22 11:39:57 +0530

    [Narendra Singh] Add Program file of file handling

---
+++ IO_Stream/File_handling/Program.cs
@@ -0,0 +1,18 @@
+﻿using File_handling;
+using FileHandlinDemo;
+using System;
+class Program
+{
+    static void Main()
+    {
+        //Text_Reader.Start();
+        //Text_Writer.Start();
+        //Stream_Writer.Start();
+        //Csv_Writer.Writer();
+        //Csv_file_reader.Reader();
+        //Json.Json_Handling();
+        //Nested_Json.Nested();
+        Read_Jason.Read();
+    }
+}
+        
